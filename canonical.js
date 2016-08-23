fe314d25c2a8539a68f5668e413ef566c8397f91
James Flacks <jamesflacks@gmail.com>
2016-08-23 10:16:14 +0100

    Create canonical.js

---
+++ canonical.js
@@ -0,0 +1,26 @@
+function cleanCanonicalURL(url) {
+  if (url.indexOf("/clearance") > -1) {
+    if (url.indexOf("refurbished") === -1 && url.indexOf("overstock") === -1) {
+      url = "/clearance-sofas";
+    }
+  }
+  if (url !== "/" && url.slice(-1) == "/") {
+
+    url = url.slice(0, -1);
+  }
+  return url;
+}
+
+function addCanonicalURL() {
+  var linkItem = document.querySelector('link[rel=canonical]') ? document.querySelector('link[rel=canonical]') : undefined;
+  if (typeof(linkItem) !== 'undefined') {
+    linkItem.parentNode.removeChild(linkItem);
+  }
+
+  var m = document.createElement('link');
+  m.rel = 'canonical';
+  m.href = "https://www.sofology.co.uk" + cleanCanonicalURL(window.location.href.split('?')[0].split("sofology.co.uk")[1]);
+  document.head.appendChild(m);
+}
+
+addCanonicalURL();
